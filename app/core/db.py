62c2dc8c8582ccdb35cf8a8e1fc2b22e524f5441
Prakarshibarya <157733726+Prakarshibarya@users.noreply.github.com>
2025-10-15 10:17:34 +0530

    Update db.py

--- app/core/db.py
+++ app/core/db.py
@@ -5,7 +5,7 @@ SQLALCHEMY_DATABASE_URL = "sqlite:///./screener.db"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL,
-    connect_args={"check_same_thread": False}  # needed for SQLite + FastAPI
+    connect_args={"check_same_thread": False}  #SQLite + FastAPI
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
